190612c0ddb95f6c3e2faa77a311bca98786de0a
YASIR ZAHID <science5661@gmail.com>
2023-07-04 01:23:33 +0500

    this file handles user and products data with all 5 API methods get one get all put patch delete

--- index.js
+++ index.js
@@ -1,46 +1,16 @@
-const http = require("http");
-const fs = require("fs");
-const { json } = require("express");
+const express = require('express');
+const morgan = require('morgan');
+const server = express();
+const productRouter = require('./routes/product')
+const userRouter = require('./routes/user')
 
-const index = fs.readFileSync("index.html", "utf-8");
-const data = JSON.parse(fs.readFileSync("data.json", "utf-8"));
-const products = data.products;
+//bodyParser
+server.use(express.json());
+server.use(morgan('default'));
+server.use(express.static('public'));
+server.use('/products',productRouter.router);
+server.use('/users',userRouter.router);
 
-
-const server = http.createServer((req, res) => {
-  console.log(req.url);
-
-  if (req.url.startsWith ("/product")) {
-    const id = req.url.split('/')[2];
-    console.log(id);
-    const product = products.find(p=>p.id==(+id));
-    console.log(product);
-    res.setHeader('Content-Type', 'text/html');
-    let modifiedIndex = index.replace('**title**', product.title)
-    .replace('**url**',product.thumbnail)
-    .replace('**price**',product.price)
-    .replace('**rating**',product.rating);
-    res.end(modifiedIndex);
-    return;
-  }
-
-  switch(req.url){
-    case "/":
-      console.log("MAIN PAGE");
-      res.setHeader("Content-Type", "text/html");
-      res.end(index);
-      break;
-    case "/api":
-      console.log("server started ");
-        res.setHeader("Content-Type", "application/json");
-        // res.end(JSON.stringify(data));
-        res.end(JSON.stringify(data));
-        break;
-    default:
-        res.writeHead(404, "not found")
-        res.end();
-
-  }
+server.listen(8080, () => {
+  console.log('server started');
 });
-
-server.listen(8080);
